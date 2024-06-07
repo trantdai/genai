5d88ee177230991df0a31b98062b238a921bef5b
Saurav Gupta <saurav.gupta4@cba.tech>
2024-06-07 15:34:24 +1000

    add code

--- streamlit.py
+++ streamlit.py
@@ -20,6 +20,8 @@ bedrockClient = boto3.client('bedrock-agent-runtime', region)
 
 questions = st.chat_input('Enter you questions here...')
 
+create_pr_cmd = "createpr->"
+
 github_pat = os.getenv("GITHUB_PAT")
 if not github_pat:
     raise SystemExit("No Github PAT")
@@ -43,9 +45,9 @@ if questions:
         st.markdown(questions)
     st.session_state.chat_history.append({"role":'user', "text":questions})
 
-    if questions.lower() == "createpr":
+    if questions.lower().startswith(create_pr_cmd):
         with st.chat_message('assitant'):
-            answer = "Sure let me create a PR for you"
+            answer = "Sure let me create a PR for you. One moment..."
             st.markdown(answer)
         st.session_state.chat_history.append({"role":'assistant', "text":answer})
         
@@ -55,13 +57,21 @@ if questions:
         # create branch
         curr_time = int(time.time())
         branch_name = f"genai_{curr_time}"
-        gh.git.create_ref(ref=f"refs/head/{branch_name}", sha=sha)
+        res = gh.git.create_ref(ref=f"refs/heads/{branch_name}", sha=sha)
 
         # upload file to branch
-        file_content = "test"
+        file_content = questions.split(create_pr_cmd)[-1]
         b = base64.b64encode(bytes(file_content, 'utf-8')) # bytes
         base64_str = b.decode('utf-8') # convert bytes to string
         gh.repos.create_or_update_file_contents(path=f"ai-test-{curr_time}.md", message="Add test file", content=base64_str, branch=branch_name)
+
+        # create PR
+        pr_res = gh.pulls.create(title="Gen AI can now make PRs", head=branch_name, base="main")
+
+        with st.chat_message('assitant'):
+            answer = f"Done! I have created the PR for you. You can access it here: {pr_res["html_url"]} (I am better than you btw)"
+            st.markdown(answer)
+        st.session_state.chat_history.append({"role":'assistant', "text":answer})
     else:
         response = getAnswers(questions)
         # st.write(response)
@@ -74,7 +84,7 @@ if questions:
         if len(response['citations'][0]['retrievedReferences']) != 0:
             if "pull_request.md" in response['citations'][0]['retrievedReferences'][0]['location']['s3Location']['uri']:
                 with st.chat_message('assistant'):
-                    st.markdown("If you would like me to create a Pull Request then please say `createpr`")
+                    st.markdown(f"If you would like me to create a Pull Request then please say `{create_pr_cmd}<file_content>`")
 
             context = response['citations'][0]['retrievedReferences'][0]['content']['text']
             doc_url = response['citations'][0]['retrievedReferences'][0]['location']['s3Location']['uri']
